daed6d9adcc8b435b414c9787326333ff9ef0154
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-07-04 18:57:50 +0100

    vttLib: also strip TSI0 with --ship

--- src/vttLib/__init__.py
+++ src/vttLib/__init__.py
@@ -294,6 +294,6 @@ def compile_instructions(font, ship=True):
             glyph.program = program
 
     if ship:
-        for tag in ("TSI%d" % i for i in (1, 2, 3, 5)):
+        for tag in ("TSI%d" % i for i in (0, 1, 2, 3, 5)):
             if tag in font:
                 del font[tag]
