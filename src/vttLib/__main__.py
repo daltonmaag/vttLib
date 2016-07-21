015bc6852cdba8d5850a54f33fb2e19c9af8e939
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-07-21 17:55:40 +0100

    vttLib.__main__: implement {dump,merge,compile} sub-commands via argparse subparsers

--- src/vttLib/__main__.py
+++ src/vttLib/__main__.py
@@ -1,62 +1,88 @@
 from __future__ import print_function, division, absolute_import
 import sys
-from fontTools.ttLib import TTFont
-from fontTools.ttx import makeOutputFileName
-from vttLib import (
-    VTTLibInvalidComposite, VTTLibError, compile_instructions,
-    update_composites,
-)
-import argparse
+from argparse import ArgumentParser
+import logging
 
-
-def parse_arguments(args):
-    if args is None:
-        args = sys.argv[1:]
-
-    parser = argparse.ArgumentParser(
-        prog="vttLib", description="Compile VTT assembly to TrueType bytecode "
-        "with FontTools")
-    parser.add_argument('infile', metavar='INPUT.ttf', help='the intput font '
-                        'containing TSI1 private table')
-    parser.add_argument('outfile', metavar='OUTPUT.ttf', nargs="?",
-                        help='the output font with compiled TrueType '
-                        'instructions.')
-    parser.add_argument('--update-composites', action='store_true', help="sync"
-                        "hronize indexes, flags and offsets of components in "
-                        "TSI1 programs with the data from the 'glyf' table")
-    parser.add_argument('--ship', action='store_true', help='remove '
-                        'all the TSI* tables from the output font.')
-
-    options = parser.parse_args(args)
-    if not options.outfile:
-        options.outfile = makeOutputFileName(options.infile, None, ".ttf")
-    return options
+from vttLib import vtt_compile, vtt_dump, vtt_merge, VTTLibArgumentError
 
 
 def main(args=None):
-    options = parse_arguments(args)
+    parser = ArgumentParser(
+        prog="python -m vttLib", description="Dump, merge or compile Visual "
+        "TrueType data in UFO3 with FontTools")
 
-    font = TTFont(options.infile)
+    parser_group = parser.add_subparsers(title="sub-commands")
+
+    parser_dump = parser_group.add_parser(
+        'dump', help="export VTT tables and 'maxp' values from TTF to UFO",
+        description="Dump VTT tables as TTX files to UFO3 'data' folder.")
+    parser_merge = parser_group.add_parser(
+        'merge', help="import VTT source data stored in UFO to TTF",
+        description="Merge VTT tables from UFO3 'data' folder to TTF font.")
+    parser_compile = parser_group.add_parser(
+        'compile', help="generate TrueType bytecode from VTT assembly",
+        description="Generate 'fpgm', 'prep', 'cvt ' and 'glyf' programs from "
+        "VTT assembly.")
+    subparsers = (parser_compile, parser_dump, parser_merge)
+
+    for subparser in subparsers:
+        group = subparser.add_mutually_exclusive_group(required=False)
+        group.add_argument('-v', '--verbose', action='store_true', help='print '
+                           'more messages to console')
+        group.add_argument('-q', '--quiet', action='store_true', help='do not '
+                           'print messages to console')
+
+    parser_merge.add_argument(
+        'infile', metavar='INPUT.ufo', help='the source UFO font containing '
+        'VTT TSI* tables in .ttx format')
+    for subparser in (parser_compile, parser_dump):
+        subparser.add_argument(
+            'infile', metavar='INPUT.ttf', help='the source TTF font '
+            'containing VTT TSI* tables')
+
+    parser_dump.add_argument(
+        'outfile', nargs='?', metavar='OUTPUT.ufo', help='the destination '
+        'UFO where to dump the VTT tables (default: INPUT + ".ufo")')
+    parser_merge.add_argument(
+        'outfile', nargs='?', metavar='OUTPUT.ttf', help='the TTF to merge '
+        'data with (default: INPUT + ".ttf")')
+
+    output_group = parser_compile.add_mutually_exclusive_group()
+    output_group.add_argument(
+        'outfile', nargs='?', metavar='OUTPUT.ttf', help='the destination TTF'
+        ' with compiled TrueType bytecode (default: '
+        'INTPUT + "#{n}.ttf").')
+    output_group.add_argument(
+        '-i', '--inplace', metavar=".bak", help="save input file in place, "
+        "and create backup with specified extension")
+    output_group.add_argument(
+        '-f', '--force-overwrite', action='store_true',
+        help="overwrite existing input file (CAUTION!)")
+
+    parser_compile.add_argument(
+        '--update-composites', action='store_true', dest="do_update_composites",
+        help="synchronize indexes, flags and offsets of components in TSI1 "
+        "programs with the data from the 'glyf' table")
+    parser_compile.add_argument(
+        '--ship', action='store_true', help='remove all the TSI* tables from '
+        'the output font.')
+
+    parser_compile.set_defaults(func=vtt_compile)
+    parser_merge.set_defaults(func=vtt_merge)
+    parser_dump.set_defaults(func=vtt_dump)
+
+    options = parser.parse_args(args)
+
+    logging.basicConfig(
+        level=("ERROR" if options.quiet else
+               "DEBUG" if options.verbose else "INFO"),
+        format="%(message)s")
 
     try:
-        if options.update_composites:
-            update_composites(font)
-        try:
-            compile_instructions(font, ship=options.ship)
-        except VTTLibInvalidComposite as e:
-            if options.update_composites:
-                raise RuntimeError("Unexpected error: %s" % e)
-            else:
-                raise VTTLibError(
-                    "Composite glyphs data in VTT source don't match the "
-                    "'glyf' table:\n%s\nTry running with --update-composites"
-                    " option." % e)
-    except VTTLibError as e:
-        print("ERROR:", e, file=sys.stderr)
-        sys.exit(1)
-    else:
-        font.save(options.outfile)
+        options.func(**vars(options))
+    except VTTLibArgumentError as e:
+        parser.error(e)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
