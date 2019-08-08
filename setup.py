f0740206957cedac21f42df642b7e51e1d74a38a
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2019-08-08 11:29:06 +0100

    Packaging housekeeping

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    use_scm_version=True,
-    # Do package discovery here until https://github.com/pypa/setuptools/issues/1136
-    # is fixed or Python 2 support is dropped.
-    package_dir={"": "src"},
-    packages=setuptools.find_packages("src"),
-)
+setuptools.setup(use_scm_version=True)
