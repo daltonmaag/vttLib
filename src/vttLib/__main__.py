6b8df334a7e4be9aa343aafa2eb6118a14c58d50
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2019-08-14 12:41:03 +0100

    Store VTT data in external dump
    This is to support storing hinting data of instances and variable fonts, which are naturally not tied to UFOs.

--- src/vttLib/__main__.py
+++ src/vttLib/__main__.py
@@ -1,103 +1,91 @@
-from __future__ import absolute_import, division, print_function
-
 import logging
+import os
 import sys
-from argparse import ArgumentParser, _VersionAction
+from argparse import ArgumentParser
 
-from vttLib import VTTLibArgumentError, vtt_compile, vtt_dump, vtt_merge
+import fontTools.ttLib
 
-
-class PkgVersionAction(_VersionAction):
-    def __call__(self, parser, namespace, values, option_string=None):
-        import pkg_resources
-
-        self.version = pkg_resources.get_distribution("vttLib").version
-        super(PkgVersionAction, self).__call__(parser, namespace, values, option_string)
+import vttLib
 
 
 def main(args=None):
     parser = ArgumentParser(
         prog="python -m vttLib",
-        description="Dump, merge or compile Visual "
-        "TrueType data in UFO3 with FontTools",
+        description="Dump, merge or compile Visual TrueType data with FontTools",
     )
-    parser.add_argument("--version", action=PkgVersionAction)
-
+    parser.add_argument("--version", action="version", version=vttLib.__version__)
     parser_group = parser.add_subparsers(title="sub-commands")
-
-    parser_dump = parser_group.add_parser(
-        "dump",
-        help="export VTT tables and 'maxp' values from TTF to UFO",
-        description="Dump VTT tables as TTX files to UFO3 'data' folder.",
+    parser_dumpfile = parser_group.add_parser(
+        "dumpfile",
+        description="Export VTT tables and 'maxp' values from a TTF into a TTX dump",
     )
-    parser_merge = parser_group.add_parser(
-        "merge",
-        help="import VTT source data stored in UFO to TTF",
-        description="Merge VTT tables from UFO3 'data' folder to TTF font.",
+    parser_mergefile = parser_group.add_parser(
+        "mergefile",
+        description="Write VTT source data stored in a TTX dump into a TTF.",
     )
     parser_compile = parser_group.add_parser(
         "compile",
-        help="generate TrueType bytecode from VTT assembly",
-        description="Generate 'fpgm', 'prep', 'cvt ' and 'glyf' programs from "
-        "VTT assembly.",
+        description=(
+            "Generate 'fpgm', 'prep', 'cvt ' and 'glyf' programs from VTT assembly."
+        ),
     )
-    subparsers = (parser_compile, parser_dump, parser_merge)
-
-    for subparser in subparsers:
+    parser_dumpfile_from_ufo = parser_group.add_parser(
+        "dumpfile_from_ufo", description="Dump VTT data from UFO3 data to a TTX dump."
+    )
+    for subparser in (
+        parser_compile,
+        parser_dumpfile,
+        parser_mergefile,
+        parser_dumpfile_from_ufo,
+    ):
         group = subparser.add_mutually_exclusive_group(required=False)
         group.add_argument(
             "-v",
             "--verbose",
             action="store_true",
-            help="print " "more messages to console",
+            help="print more messages to console",
         )
         group.add_argument(
             "-q",
             "--quiet",
             action="store_true",
-            help="do not " "print messages to console",
+            help="do not print messages to console",
         )
 
-    parser_merge.add_argument(
+    parser_dumpfile.add_argument("infile", metavar="INPUT.ttf")
+    parser_dumpfile.add_argument("outfile", nargs="?", metavar="OUTPUT.ttx")
+    parser_dumpfile.set_defaults(func=vttLib.vtt_dump_file)
+
+    parser_mergefile.add_argument("infile", metavar="INPUT.ttx")
+    parser_mergefile.add_argument("outfile", metavar="OUTPUT.ttf")
+    parser_mergefile.set_defaults(func=vttLib.vtt_merge_file)
+
+    parser_compile.add_argument(
         "infile",
-        metavar="INPUT.ufo",
-        help="the source UFO font containing " "VTT TSI* tables in .ttx format",
+        metavar="INPUT.ttf",
+        help="the source TTF font containing VTT TSI* tables",
     )
-    for subparser in (parser_compile, parser_dump):
-        subparser.add_argument(
-            "infile",
-            metavar="INPUT.ttf",
-            help="the source TTF font " "containing VTT TSI* tables",
-        )
-
-    parser_dump.add_argument(
-        "outfile",
-        nargs="?",
-        metavar="OUTPUT.ufo",
-        help="the destination "
-        'UFO where to dump the VTT tables (default: INPUT + ".ufo")',
+    parser_compile.add_argument(
+        "--ship",
+        action="store_true",
+        help="remove all the TSI* tables from the output font.",
     )
-    parser_merge.add_argument(
-        "outfile",
-        nargs="?",
-        metavar="OUTPUT.ttf",
-        help="the TTF to merge " 'data with (default: INPUT + ".ttf")',
-    )
-
+    parser_compile.set_defaults(func=vttLib.vtt_compile)
     output_group = parser_compile.add_mutually_exclusive_group()
     output_group.add_argument(
         "outfile",
         nargs="?",
         metavar="OUTPUT.ttf",
-        help="the destination TTF"
-        " with compiled TrueType bytecode (default: "
-        'INTPUT + "#{n}.ttf").',
+        help=(
+            "the destination TTF with compiled TrueType bytecode (default: "
+            'INTPUT + "#{n}.ttf").'
+        ),
     )
     output_group.add_argument(
         "-i",
         "--inplace",
         metavar=".bak",
-        help="save input file in place, " "and create backup with specified extension",
+        help="save input file in place, and create backup with specified extension",
     )
     output_group.add_argument(
         "-f",
@@ -106,26 +94,26 @@ def main(args=None):
         help="overwrite existing input file (CAUTION!)",
     )
 
-    parser_compile.add_argument(
-        "--ship",
-        action="store_true",
-        help="remove all the TSI* tables from " "the output font.",
-    )
-
-    parser_compile.set_defaults(func=vtt_compile)
-    parser_merge.set_defaults(func=vtt_merge)
-    parser_dump.set_defaults(func=vtt_dump)
+    parser_dumpfile_from_ufo.add_argument("infile", metavar="SOURCE.ufo")
+    parser_dumpfile_from_ufo.add_argument("outfile", nargs="?", metavar="OUTPUT.ttx")
+    parser_dumpfile_from_ufo.set_defaults(func=vttLib.vtt_move_ufo_data_to_file)
 
     options = parser.parse_args(args)
 
-    logging.basicConfig(
-        level=("ERROR" if options.quiet else "DEBUG" if options.verbose else "INFO"),
-        format="%(name)s: %(levelname)s: %(message)s",
-    )
+    if "quiet" in options or "verbose" in options:
+        logging.basicConfig(
+            level=(
+                "ERROR" if options.quiet else "DEBUG" if options.verbose else "INFO"
+            ),
+            format="%(name)s: %(levelname)s: %(message)s",
+        )
 
     try:
-        options.func(**vars(options))
-    except VTTLibArgumentError as e:
+        if "func" in options:
+            options.func(**vars(options))
+        else:
+            parser.print_help()
+    except vttLib.VTTLibArgumentError as e:
         parser.error(e)
 
 
